4d7b524ad0d985dbf09c584e1077cac8547e59bc
legoadventurer <vishruthrayakuntapalle@gmail.com>
2025-12-03 16:50:06 -0800

    Exersizes 5

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/members/vishruth/ProgrammingBoard.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.members.vishruth;
+
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+
+public class ProgrammingBoard {
+    DigitalChannel touchSensor;
+
+
+}
