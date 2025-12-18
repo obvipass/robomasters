2591d90c52ae3a6624f997971e2b6eca2e9002a5
Lightning272 <omsavani51@gmail.com>
2025-12-17 19:06:49 -0800

    Exercises

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/members/om/ColorSensorCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/members/om/ColorSensorCode.java
@@ -3,7 +3,6 @@ package org.firstinspires.ftc.teamcode.members.om;
 import android.app.Activity;
 import android.graphics.Color;
 import android.view.View;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
@@ -14,7 +13,6 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 
 @TeleOp(name = "Om - Color Sensor Code")
-@Disabled
 public class ColorSensorCode extends LinearOpMode {
 
     NormalizedColorSensor colorSensor;
