7718f4f6ff06369030ecf341670aa0aa7d28f23b
Saksham Singh <iamsaksham603@gmail.com>
2026-01-11 19:36:07 -0800

    even more random merge/package errors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/members/ishani/ISObstacleAvoidance.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/members/ishani/ISObstacleAvoidance.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.members.om;
+package org.firstinspires.ftc.teamcode.members.ishani;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
